3685642301edbd79cb58b41a3abe86397e83c1c8
Damian Peckett <damian@pecke.tt>
2023-12-06 10:48:28 +0100

    refactor: tidy things up a bit

--- btree.c
+++ btree.c
@@ -1,6 +1,21 @@
-/*	$OpenBSD: btree.c,v 1.38 2017/05/26 21:23:14 sthen Exp $ */
-
-/*
+/* SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2023 Damian Peckett <damian@pecke.tt>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Based on btree.c from the ldapd project:
+ * 
  * Copyright (c) 2009, 2010 Martin Hedenfalk <martin@bzero.se>
  *
  * Permission to use, copy, modify, and distribute this software for any
